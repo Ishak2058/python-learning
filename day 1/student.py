2fcd9acb1c6d293d4717f0c71459e4ef7e81b1a9
Ishak BK <bkishak347@gmail.com>
2026-05-12 18:58:34 +0545

    day 1 learning python folder

---
+++ day 1/student.py
@@ -0,0 +1,26 @@
+# Simple Python script to display text
+import numbers
+file = open("../../ishak.txt","r")
+total = 0
+count = 0
+
+highest = 0
+lowest = 999
+
+for line in file:
+    number = int(line)
+
+    total = total + number
+    count = count + 1
+    if number > highest:
+        highest = number
+    if number < lowest:
+        lowest = number
+average = total /count
+
+
+print("Total number of numbers: ", total)
+print("Average number of numbers: ", average)
+print("Highest number: ", highest)
+print("Lowest number: ", lowest)
+file.close()
